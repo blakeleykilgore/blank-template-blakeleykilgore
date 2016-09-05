57babe1fe40736e9bab9aed445c42e0dbca4a8c0
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-05 13:02:25 -0500

    Add initial HTML h1 and p and CSS reset rule

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<?php 
-if ( have_posts() ) {
-    while ( have_posts() ) {
-        the_post(); 
-    } // end while
-} // end if
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Blakeley Kilgore Blank Template</title>
+</head>
+<body>
+	<h1>Blank WordPress Template</h1>
+    <p>This is my WordPress template.</p>
+</body>
+</html>
+
