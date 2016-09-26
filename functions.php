d12b76d47b9ce85189e3667e48fdba5cab33dae2
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 16:58:49 -0500

    Add three widgets for the footer to functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,4 @@
-<?php
 /*-------------- Enable Widgets--------------- */
-
 function blank_widgets_init() {
 	register_sidebar( array(
 		'name' => ('First Widget'),
@@ -11,6 +9,36 @@ function blank_widgets_init() {
 		'before_title' => '<h2>',
 		'after_title' => '</h2>'
 		));
-	}
+  /*--- New Widget --- */
+	register_sidebar( array(
+		'name' => ('Footer Widget One'),
+		'id' => 'footer-widget-one',
+		'description' => 'First widget for our footer',
+		'before_widget' => '<div class="widget-footer">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>'
+		));
+	/*--- Second New Widget --- */
+	register_sidebar( array(
+		'name' => ('Footer Widget Two'),
+		'id' => 'footer-widget-two',
+		''description' => 'Second widget for our footer',
+		'before_widget' => '<div class="widget-footer">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>'
+		));
+	/*--- Third New Widget --- */
+	register_sidebar( array(
+		'name' => ('Footer Widget Three'),
+		'id' => 'footer-widget-three',
+		'description' => 'Third widget for our footer',
+		'before_widget' => '<div class="widget-footer">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>'
+		));
+
+}
 add_action('widgets_init', 'blank_widgets_init');
-?>
