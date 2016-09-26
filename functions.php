343fadb3bff35d8ab9baeefe86e1c84ce0ec0522
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 17:45:32 -0500

    Enable thumbnails in functions.php

--- functions.php
+++ functions.php
@@ -46,4 +46,7 @@ register_sidebar( array(
 add_action('widgets_init', 'blank_widgets_init');
 /*-------------- Enable Menu --------------- */
 add_theme_support('menus');
+
+/*--- Enable Post Thumbnails ---*/
+add_theme_support( 'post-thumbnails' ); 
 ?>
