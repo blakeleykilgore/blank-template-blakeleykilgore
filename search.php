4b3537ac3f76f79b08705b305c705f63de50cc2e
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 17:38:16 -0500

    Add initial shell to search.php

---
+++ search.php
@@ -0,0 +1,15 @@
+<?php
+/* Blakeley Kilgore Blank Template: Search Page */
+
+get_header(); ?>
+
+	<div class="row">
+		<div class="eight columns">
+
+		</div>
+		<div class="four columns">
+			<?php get_sidebar(); ?>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
