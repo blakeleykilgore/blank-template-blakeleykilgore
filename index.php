7a5cf6d38d63d85d5d942983604b960e2b5b3c9f
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 15:31:03 -0500

    Add HTML5 syntax to index.php

--- index.php
+++ index.php
@@ -6,7 +6,15 @@
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
-	<h1>Blank WordPress Template</h1>
-    <p>This is my WordPress template.</p>
+	<header>
+			<h1>Blank WordPress Template</h1>
+			<p>This is my WordPress template.</p>
+		</header>
+		<section>
+			<p>Body content goes here.</p>
+		</section>
+		<footer>
+			<p>Footer content goes here.</p>
+		</footer>
 </body>
 </html>
