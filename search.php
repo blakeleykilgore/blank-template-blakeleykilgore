ab4f97c9a0b82b4f3d6f801b39ce66eca6b19298
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 17:38:48 -0500

    Add functionality to search.php

--- search.php
+++ search.php
@@ -5,7 +5,13 @@ get_header(); ?>
 
 	<div class="row">
 		<div class="eight columns">
-
+			<h1><?php printf(__('Search Results for: %s'), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			<?php if (have_posts()) :
+				while (have_posts()) : the_post(); ?> 									<h2><?php the_title(); ?></h2>
+					<?php the_content();
+				endwhile;
+			endif;
+		?>
 		</div>
 		<div class="four columns">
 			<?php get_sidebar(); ?>
