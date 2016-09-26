da602954340399d8b683f6e6cd414c6d7a673a99
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 15:29:08 -0500

    Add link in HTML and CSS to skeleton.css

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Blakeley Kilgore Blank Template</title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
 	<h1>Blank WordPress Template</h1>
