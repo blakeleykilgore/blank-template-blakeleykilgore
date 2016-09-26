0adc7e5ee140b03291c2d87e27f4aef79a279f83
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 16:25:27 -0500

    Add a 3 column sidebar section to page.php

--- page.php
+++ page.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
 	<div class="row">
-		<div class="twelve columns">
-<!-- BEGIN PAGE PHP -->
+		<div class="nine columns">
 			<?php if (have_posts()) :
-				/* OUR DATA CONTEXT IS DEFINED 	*/
-				while (have_posts()) : the_post(); ?>
+while (have_posts()) : the_post(); ?>
 					<h2><?php the_title(); ?></h2>
 					<?php the_content();
 				endwhile;
 			endif; ?>
-<!-- END PAGE PHP -->
 		</div>
+<!-- BEGIN SIDEBAR -->
+		<div class="three columns">
+			<?php get_sidebar(); ?>
+		</div>
+<!-- END SIDEBAR -->
 	</div>
 <?php get_footer(); ?>
