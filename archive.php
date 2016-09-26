9ca00d5cf9d426e92cb66c3765e533160e269a2f
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 17:29:02 -0500

    Add initial shell to archive.php

---
+++ archive.php
@@ -0,0 +1,11 @@
+<?php
+/* Template Name: Archive Page */
+
+get_header(); ?>
+	<div class="row">
+		<div class="twelve columns">
+			<h2>Archive</h2>
+			<p>Archive content goes here.</p>
+		</div>
+	</div>
+<?php get_footer(); ?>
