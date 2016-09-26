1d6ffad321b00bca9a4db644e73f42b715eb1e4d
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 17:01:02 -0500

    Add three widgets to footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,16 @@
-      <footer class="row">
-				<div class="twelve columns">
-					<p>Footer content goes here.</p>
-				</div>
-			</footer>
-		</div> <!-- ends container -->
-	<?php wp_footer(); ?>
-	</body>
+<footer class="row">
+<div class="four columns">
+<?php dynamic_sidebar('footer-widget-one'); ?>
+</div>
+<div class="four columns">
+<?php dynamic_sidebar('footer-widget-two'); ?>
+</div>
+<div class="four columns">
+<?php dynamic_sidebar('footer-widget-three'); ?>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+
+</body>
 </html>
