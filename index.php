9bf11c20b71ba6db4161f11149df271e251f6dab
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-26 19:10:46 -0500

    Add permalinks to posts' titles and "read more"

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
 			if ( have_posts() ) {
 			    while ( have_posts() ) {
 			        the_post(); ?>
-							<h3><?php the_title(); ?></h3>
-							<?php
+							<!-- ADD LINK TO THE TITLE -->
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <?php
 							if ( has_post_thumbnail() ) {
 								the_post_thumbnail('thumbnail');
-							} 
-							the_excerpt();
+							}
+							the_excerpt(); ?>
+							<p><a href="<?php the_permalink(); ?>">Read More</a></p>
+							<?php
 					} // end while
 			} // end if
 			?>
